4310fca3946f84b664cdaf4a7ddcffa73bc33acb
Simon Hischier <simon_hischier@hotmail.com>
2025-04-30 00:00:16 +0200

    now with music

--- projectboost/player.gd
+++ projectboost/player.gd
@@ -2,8 +2,11 @@ extends RigidBody3D
 
 ## How much vertical force to apply when moving.
 @export_range(750.0, 3000.0) var thrust: float = 1000.0;
-
 @export_range(20.0, 200.0) var torque_trust: float = 100.0;
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+
+var is_transition: bool = false;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,16 +20,28 @@ func _process(delta: float) -> void:
 		apply_torque(Vector3(0.0, 0.0, -torque_trust) * delta);
 
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("Goal"):
+	if (is_transition):
+		return
+		
+	if (body.is_in_group("Goal")):
 		complete_level(body.file_path);
 		
-	if body.is_in_group("Hazard"):
+	if (body.is_in_group("Hazard")):
 		crash_sequence();
 
 func crash_sequence() -> void:
 	print("KABOOM");
-	get_tree().reload_current_scene();
+	explosion_audio.play();
+	is_transition = true;
+	set_process(false);
+	var tween = create_tween();
+	tween.tween_interval(2.5);
+	tween.tween_callback(get_tree().reload_current_scene);
 
 func complete_level(next_level_file: String) -> void:
 	print("Level Complete");
-	get_tree().change_scene_to_file(next_level_file)
+	success_audio.play();
+	is_transition = true;
+	var tween = create_tween();
+	tween.tween_interval(1.0);
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file));
