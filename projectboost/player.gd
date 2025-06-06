3b825a74d6ab5675787d83cf0b986e5c3456b8f7
Simon Hischier <simon_hischier@hotmail.com>
2025-06-06 10:52:59 +0200

    added particles and updated mesh

--- projectboost/player.gd
+++ projectboost/player.gd
@@ -6,6 +6,11 @@ extends RigidBody3D
 @onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
 @onready var success_audio: AudioStreamPlayer = $SuccessAudio
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
+@onready var left_booster_particles_2: GPUParticles3D = $LeftBoosterParticles2
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
+@onready var success_particles: GPUParticles3D = $SuccessParticles
 
 var is_transition: bool = false;
 
@@ -13,16 +18,24 @@ var is_transition: bool = false;
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_force(basis.y * delta * thrust);
+		booster_particles.emitting = true;
 		if !rocket_audio.playing:
 			rocket_audio.play();
 	else:
 		rocket_audio.stop();
+		booster_particles.emitting = false;
 	
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torque_trust) * delta);
+		right_booster_particles.emitting = true;
+	else:
+		right_booster_particles.emitting = false;
 		
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -torque_trust) * delta);
+		left_booster_particles_2.emitting = true;
+	else:
+		left_booster_particles_2.emitting = false;
 
 func _on_body_entered(body: Node) -> void:
 	if (is_transition):
@@ -36,6 +49,7 @@ func _on_body_entered(body: Node) -> void:
 
 func crash_sequence() -> void:
 	print("KABOOM");
+	explosion_particles.emitting = true;
 	explosion_audio.play();
 	is_transition = true;
 	set_process(false);
@@ -45,6 +59,7 @@ func crash_sequence() -> void:
 
 func complete_level(next_level_file: String) -> void:
 	print("Level Complete");
+	success_particles.emitting = true;
 	success_audio.play();
 	is_transition = true;
 	var tween = create_tween();
