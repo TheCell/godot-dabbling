239579acda4f48847bb6b62fe575d6a03a8ebaa3
Simon Hischier <simon_hischier@hotmail.com>
2025-06-06 11:40:45 +0200

    close game on esc

--- projectboost/player.gd
+++ projectboost/player.gd
@@ -36,6 +36,9 @@ func _process(delta: float) -> void:
 		left_booster_particles_2.emitting = true;
 	else:
 		left_booster_particles_2.emitting = false;
+		
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit();
 
 func _on_body_entered(body: Node) -> void:
 	if (is_transition):
