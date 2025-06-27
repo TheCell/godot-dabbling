3f3a0c7220abe10f41de65257299a54e7fceab83
Simon Hischier <simon_hischier@hotmail.com>
2025-06-27 11:53:23 +0200

    added player falling faster

--- robo-rampage/Player/player.gd
+++ robo-rampage/Player/player.gd
@@ -2,7 +2,9 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+@export var jump_height: float = 1.0;
+@export var fall_multiplier: float = 2.5;
 
 var mouse_motion := Vector2.ZERO;
 
@@ -16,12 +18,15 @@ func _physics_process(delta: float) -> void:
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if (velocity.y >= 0):
+			velocity += get_gravity() * delta
+		else:
+			velocity += get_gravity() * delta * fall_multiplier
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+		velocity.y = sqrt(jump_height * 2.0 * -get_gravity().y);
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
