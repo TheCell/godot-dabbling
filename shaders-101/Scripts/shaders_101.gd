e653071de3926743f6eb83d653ced99302f59dbf
Simon Hischier <simon_hischier@hotmail.com>
2025-07-13 12:08:01 +0200

    animating vertices

--- shaders-101/Scripts/shaders_101.gd
+++ shaders-101/Scripts/shaders_101.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-func _process(delta: float) -> void:
-	var time = Time.get_ticks_msec() / 1000.0;
-	
-	print("TIME: " + str(time));
+#func _process(delta: float) -> void:
+	#var time = Time.get_ticks_msec() / 1000.0;
+	#var a = (sin(time) + 1.0) * 0.5;
+	#
+	#print("TIME: " + str(time));
+	#print("a: " + str(a));
