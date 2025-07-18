ad84a57f88b59261272451c217c2c22bc88b615f
Simon Hischier <simon_hischier@hotmail.com>
2025-07-18 11:27:52 +0200

    basic shader

--- spatial_shaders_starter_project/scripts/spatial_shaders.gd
+++ spatial_shaders_starter_project/scripts/spatial_shaders.gd
@@ -1,5 +1,14 @@
 extends Node3D
 
+@onready var sphere_example: MeshInstance3D = $SphereExample
+
+
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+	
+	if (Input.is_action_just_pressed("set_color")):
+		var sphere_mat := sphere_example.get_active_material(0);
+		var random_color = Vector3(randf(), randf(), randf());
+		sphere_mat.set_shader_parameter("sphere_color", random_color);
+	
