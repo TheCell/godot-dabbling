93650396f61546398975be96d0c6091a7dc2a111
Simon Hischier <simon_hischier@hotmail.com>
2025-07-25 13:01:34 +0200

    floowing camera

--- multiplayer-dabbling/player/player.gd
+++ multiplayer-dabbling/player/player.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 
+@export var player_camera: PackedScene;
+@export var camera_height := 315;
+
 @export var player_sprite: AnimatedSprite2D;
 
 @export var movement_speed := 300;
 @export var gravity := 30;
 @export var jump_strength := 600;
 @export var max_jumps = 1;
-var jump_count := 0;
 
 @onready var initial_sprite_scale = player_sprite.scale;
+
+var jump_count := 0;
+var camera_instance;
+
+func _ready() -> void:
+	camera_instance = player_camera.instantiate();
+	camera_instance.global_position.y = camera_height;
+	get_tree().current_scene.add_child.call_deferred(camera_instance);
+
+func _process(delta: float) -> void:
+	camera_instance.global_position.x = global_position.x;
 
 func _physics_process(_delta: float) -> void:
 	var horizontal_input := Input.get_action_strength("move_right") - Input.get_action_strength("move_left");
