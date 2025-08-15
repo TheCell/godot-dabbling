074bbecab664b7c51952d14da4bdb626660d76c3
Simon Hischier <simon_hischier@hotmail.com>
2025-08-15 15:59:27 +0200

    opening and closing doors

---
+++ multiplayer-dabbling/interactables/door/door.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var is_open := false
+@export var door_open: Sprite2D
+@export var door_closed: Sprite2D
+@export var collider: CollisionShape2D
+
+func activate(state) -> void:
+	if not multiplayer.is_server():
+		return
+	is_open = state
+	set_door_properties()
+
+func set_door_properties() -> void:
+	#print_debug(is_open)
+	door_open.visible = is_open
+	door_closed.visible = !is_open
+	collider.set_deferred("disabled", is_open)
+
+func _on_multiplayer_synchronizer_synchronized() -> void:
+	set_door_properties()
