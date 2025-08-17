253bda1d4d170721d5c9c136bbdbb845f61b5b59
Simon Hischier <simon_hischier@hotmail.com>
2025-08-17 16:07:02 +0200

    opening door with key

--- multiplayer-dabbling/interactables/chest/chest.gd
+++ multiplayer-dabbling/interactables/chest/chest.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var key_scene: PackedScene
+@export var key_spawn: Node2D
 @export var is_locked := true
 @export var chest_locked: Sprite2D
 @export var chest_unlocked: Sprite2D
@@ -10,6 +12,8 @@ func _on_multiplayer_synchronizer_delta_synchronized() -> void:
 func _on_interactable_interacted() -> void:
 	if is_locked:
 		is_locked = false
+		var key = key_scene.instantiate()
+		key_spawn.add_child(key);
 		set_chest_properties()
 
 func set_chest_properties() -> void:
