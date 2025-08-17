1a2efc20be68be0692e787cd119cedd0664c9016
Simon Hischier <simon_hischier@hotmail.com>
2025-08-17 17:03:19 +0200

    made crate multiplayer workable

--- multiplayer-dabbling/interactables/pushable_objects/pushable_object.gd
+++ multiplayer-dabbling/interactables/pushable_objects/pushable_object.gd
@@ -1,11 +1,29 @@
 extends RigidBody2D
 
+class_name PushableObject
+
+var requested_authority = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if not multiplayer.is_server():
+		freeze = true
+		#set_deferred("freeze", true)
 
+func push(impulse, point) -> void:
+	if is_multiplayer_authority():
+		apply_impulse(impulse, point)
+	else:
+		if not requested_authority:
+			requested_authority = true
+			request_authority.rpc_id(get_multiplayer_authority(), multiplayer.get_unique_id())
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@rpc("any_peer", "call_remote", "reliable")
+func request_authority(id) -> void:
+	set_pushable_owner.rpc(id)
+
+@rpc("authority", "call_local", "reliable")
+func set_pushable_owner(id) -> void:
+	requested_authority = false
+	set_multiplayer_authority(id)
+	set_deferred("freeze", multiplayer.get_unique_id() != id)
